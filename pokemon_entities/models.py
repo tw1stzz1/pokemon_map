7cc3420593e7268524db6bad1692a2881fe43102
Tkaz <egor4ig95@mail.ru>
2024-08-03 14:20:10 +0300

    Update models.py

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
-    image = models.ImageField(null=True)
+    image = models.ImageField(blank=True)
 
     def __str__(self):
         return self.title
@@ -13,3 +13,5 @@ class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
     latitude = models.FloatField()
     longitude = models.FloatField()
+    appeared_at = 
+    disappeared_at = 
